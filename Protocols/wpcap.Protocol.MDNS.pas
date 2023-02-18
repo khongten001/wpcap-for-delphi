9731ccafdad4035283a7aa0bfec9afc4fecee247
amancini <syrio84@gmail.com>
2023-02-18 20:23:11 +0100

    Protocol detections
    DNS by port
    L2TP magic coockie
    LLMNR by port
    MDNS by port
    NTP by port
    UDP by IPPROTO

---
+++ Protocols/wpcap.Protocol.MDNS.pas
@@ -0,0 +1,58 @@
+﻿unit wpcap.Protocol.MDNS;
+
+interface
+
+uses wpcap.Protocol.DNS,wpcap.Conts;
+
+type
+  /// <summary>
+  ///  mDNS (Multicast DNS) protocol class, subclass of TWPcapProtocolDNS.
+  /// </summary>
+  TWPcapProtocolMDNS = Class(TWPcapProtocolDNS)
+  public
+    /// <summary>
+    ///  Returns the default port number for mDNS protocol, which is 5353.
+    /// </summary>
+    class Function DefaultPort: Word; override;
+    
+    /// <summary>
+    ///  Returns the unique ID for mDNS protocol, which is 6.
+    /// </summary>
+    class Function IDDetectProto: Integer; override;
+    
+    /// <summary>
+    ///  Returns the name of the mDNS protocol, which is "Multicast DNS".
+    /// </summary>
+    class function ProtoName: String; override;
+    
+    /// <summary>
+    ///  Returns the acronym name for the mDNS protocol, which is "MDNS".
+    /// </summary>
+    class function AcronymName: String; override;
+  end;
+
+implementation
+
+{ TWPcapProtocolMDNS }
+
+class function TWPcapProtocolMDNS.DefaultPort: Word;
+begin
+  Result := PROTO_MDNS_PORT;
+end;
+
+class function TWPcapProtocolMDNS.IDDetectProto: Integer;
+begin
+  Result := DETECT_PROTO_MDNS
+end;
+
+class function TWPcapProtocolMDNS.ProtoName: String;
+begin
+  Result := 'Multicast DNS';
+end;
+
+class function TWPcapProtocolMDNS.AcronymName: String;
+begin
+  Result := 'MDNS';
+end;
+
+end.
